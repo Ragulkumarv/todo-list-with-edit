f6dd8e9f97e08741ac6f1c29fda19dd9d4bd6ea2
Ragul <ragulkumarv333@gmail.com>
2025-02-22 10:34:52 +0530

    Revert "speed-insights added"
    This reverts commit 24a32266534f7f2e8418227bc1ff935eac7f75db.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import "./App.css";
 import ToDoList from "./components/TodoListWithEdit";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
-  return (
-    <>
-      <ToDoList />
-      <SpeedInsights />
-    </>
-  );
+  return <ToDoList />;
 }
 
 export default App;
