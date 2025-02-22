24a32266534f7f2e8418227bc1ff935eac7f75db
Ragul <ragulkumarv333@gmail.com>
2025-02-22 10:33:51 +0530

    speed-insights added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import ToDoList from "./components/TodoListWithEdit";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
-  return <ToDoList />;
+  return (
+    <>
+      <ToDoList />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default App;
